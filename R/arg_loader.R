e8287be6584a9de21c4195af81bef9b7d6bdf2fc
aar126 <transcriptionfactory@gmail.com>
2023-05-04 13:08:44 -0400

    optparse

--- R/arg_loader.R
+++ R/arg_loader.R
@@ -1,12 +1,4 @@
-arglist = list(x_path = list(options = c("x", "x_path"), default = ""),
-               y_path = list(options = c("y", "y_path"), default = ""),
-               data_path = list(options = c("d", "data", "data_path"),
-                                default = ""),
-               yaml_path = list(options = c("yaml", "yaml_path", "p"),
-                                default = ""),
-               out_path = list(options = c("o", "out_path"), default = "/"),
-               coarse_grid = list(options = c("c", "coarse_grid", "coarseGrid"),
-                                  default = F))
+
 
 arg_loader = function(args, default_arglist) {
 
@@ -23,3 +15,28 @@ arg_loader = function(args, default_arglist) {
   }
   return(arglist)
 }
+
+
+
+# runER_args = list(
+#   optparse::make_option(c("-x", "--x_path"), action = "store",
+#                         default = " ", help = "Path to X data"),
+#   optparse::make_option(c("-y", "--y_path"), action = "store",
+#                         default = " ", help = "Path to Y data"),
+#   optparse::make_option(c("-d", "--data_path"), action = "store",
+#                         default = " ", help = "Path to data"),
+#   optparse::make_option(c("-p", "--yaml_path"), action = "store",
+#                         default = " ", help = "Path to yaml file"),
+#   optparse::make_option(c("-c", "--coarse_grid"), action = "store_true",
+#                         default = F, help = "Run coarse grid search of lamda/deltas")
+# )
+
+# arglist = list(x_path = list(options = c("x", "x_path"), default = ""),
+#                y_path = list(options = c("y", "y_path"), default = ""),
+#                data_path = list(options = c("d", "data", "data_path"),
+#                                 default = ""),
+#                yaml_path = list(options = c("yaml", "yaml_path", "p"),
+#                                 default = ""),
+#                out_path = list(options = c("o", "out_path"), default = "/"),
+#                coarse_grid = list(options = c("c", "coarse_grid", "coarseGrid"),
+#                                   default = F))
