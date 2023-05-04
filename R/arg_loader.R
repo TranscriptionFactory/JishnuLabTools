a2919100b0649120df083b80e4732b96a39b5a3f
aar126 <transcriptionfactory@gmail.com>
2023-05-04 09:39:39 -0400

    added vignettes

--- R/arg_loader.R
+++ R/arg_loader.R
@@ -1,13 +1,20 @@
-# arglist = list(x_path = list("options" = c("x", "x_path"), "default" = ""),
-#                y_path = list("options" = c("y", "y_path"), "default" = ""),
-#                out_path = list("options" = c("o", "out_path"), "default" = "/"),
-#                run_all = list("options" = c("a", "run_all"), "default" = F))
+# arglist = list(x_path = list(options = c("x", "x_path"), default = ""),
+#                y_path = list(options = c("y", "y_path"), default = ""),
+#                data_path = list(options = c("d", "data", "data_path"),
+#                                 default = ""),
+#                yaml_path = list(options = c("yaml", "yaml_path", "p"),
+#                                 default = ""),
+#                out_path = list(options = c("o", "out_path"), default = "/"),
+#                coarse_grid = list(options = c("c", "coarse_grid", "coarseGrid"),
+#                                   default = F))
 
 arg_loader = function(args, default_arglist) {
 
   arglist = default_arglist
   for(a in arglist) {
-    if (length(intersect(a$options, names(args))) > 0) {
+    if (any(lapply(a$options, function(x) any(stringr::str_detect(names(args),
+                                                              pattern = x))))) {
+      #length(intersect(a$options, names(args))) > 0) {
       arglist[a] = unlist(args[intersect(a$options, names(args))[1]])
     } else {
       # empty argument; set as default
