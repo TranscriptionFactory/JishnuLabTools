c8a74af38979a082898625e9801d1bd6747b2ab6
Transcription Factory <transcriptionfactory@gmail.com>
2023-05-12 08:16:08 -0400

    Update set_k_folds.R

--- R/set_k_folds.R
+++ R/set_k_folds.R
@@ -4,7 +4,7 @@ set_k_folds = function(er_input){
   # load the y from input
   y = as.matrix(read.csv(er_input$y_path), row.names = 1)
   # sample size
-  n = length(y)
+  n = min(length(y), nrow(y))
 
   # default is 10 folds unless we have small sample size
   k = 10
