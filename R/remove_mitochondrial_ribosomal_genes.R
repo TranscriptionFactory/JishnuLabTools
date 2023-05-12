68a6d5e38f92b1a45ef80911b6c218e883aa5cf8
Transcription Factory <transcriptionfactory@gmail.com>
2023-05-12 00:36:39 -0400

    Update remove_mitochondrial_ribosomal_genes.R

--- R/remove_mitochondrial_ribosomal_genes.R
+++ R/remove_mitochondrial_ribosomal_genes.R
@@ -2,11 +2,13 @@
 remove_mitochondrial_ribosomal_genes = function(df) {
 
   # check if genes have cluster names e.g C1.RPS prepended
-  cluster_names = grepl("^[[:alnum:]].\\d+.HSP|RPS[[:digit:]]|RPL[[:digit:]]|RPLP[[:digit:]]|RPSA|RPS|MT|MTRNR|MT4|MT3|MT2A|MT1E|MT1M|MT1A|MT1B|MT1F|MT1G|MT1H|MTND|ATP", colnames(df))
+  cluster_names = which(grepl("^[[:alnum:]].\\d+.HSP|RPS[[:digit:]]|RPL[[:digit:]]|RPLP[[:digit:]]|RPSA|RPS|MT|MTRNR|MT4|MT3|MT2A|MT1E|MT1M|MT1A|MT1B|MT1F|MT1G|MT1H|MTND|ATP", colnames(df)))
 
-  no_cluster_names = grepl("^HSP|RPS[[:digit:]]|RPL[[:digit:]]|RPLP[[:digit:]]|RPSA|RPS|MT|MTRNR|MT4|MT3|MT2A|MT1E|MT1M|MT1A|MT1B|MT1F|MT1G|MT1H|MTND|ATP", colnames(df))
+  no_cluster_names = which(grepl("^HSP|RPS[[:digit:]]|RPL[[:digit:]]|RPLP[[:digit:]]|RPSA|RPS|MT|MTRNR|MT4|MT3|MT2A|MT1E|MT1M|MT1A|MT1B|MT1F|MT1G|MT1H|MTND|ATP", colnames(df)))
 
-  if (length(cluster_names) > 0) {
+  if (length(cluster_names) == 0 && length(no_cluster_names) == 0) {
+    return(df)
+  } else if (length(cluster_names) > 0) {
     df = df[, -which(cluster_names)]
   } else {
     df = df[, -which(no_cluster_names)]
