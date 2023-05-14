b60e276279604fe9a034daa6f5344828a824c0d3
aar126 <transcriptionfactory@gmail.com>
2023-05-14 14:44:25 -0400

    error handlign when file isnt found

--- R/load_output_from_runs.R
+++ R/load_output_from_runs.R
@@ -5,9 +5,20 @@
 load_output_from_runs = function(runs, filename) {
                                 #function_to_read_files = JishnuLabTools:::safely_load_obj_from_path) {
 
-  runs_with_desired_file = sapply(runs, function(x)
+  runs_with_desired_file = lapply(runs, function(x)
     list.files(x, full.names = T)[stringr::str_which(list.files(x),
                                                      pattern = filename)])
+
+  # remove any entries that didn't find a match
+  for (l in 1:length(runs_with_desired_file)) {
+    if (length(runs_with_desired_file[[l]]) == 0) {
+      runs_with_desired_file[[l]] = NULL
+    } else if (length(runs_with_desired_file[[l]]) > 1) {
+      # if theres more than one match, just pick first
+      runs_with_desired_file[[l]] = runs_with_desired_file[[l]][1]
+    }
+  }
+
   if ( length(runs_with_desired_file) == 0 ) {
     return(FALSE)
   } else {
